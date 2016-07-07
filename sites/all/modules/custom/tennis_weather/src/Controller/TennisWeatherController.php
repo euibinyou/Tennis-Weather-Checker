9b51f8fab9b31cd4d0c246b3790c4e825050356a
Eric Y <Eric Y>
2016-07-07 15:23:03 -0400

    Add error handling when nothing returned from Weather Underground API (eg. internet connectivity issues)

--- sites/all/modules/custom/tennis_weather/src/Controller/TennisWeatherController.php
+++ sites/all/modules/custom/tennis_weather/src/Controller/TennisWeatherController.php
@@ -45,6 +45,13 @@ class TennisWeatherController {
     }
     $parsed_json = json_decode($response);
 
+    // Error handling if nothing returend from Weather Underground API
+    if ($parsed_json === NULL) {
+      drupal_set_message('Error: nothing returned from Weather Underground API (probably an internet connectivity problem)', 'error');
+      $element['#debug_info']['error'] = True;
+      return $element;
+    }
+
     // Error handling for Weather Underground API response
     if (property_exists($parsed_json->{'response'}, 'error')) {
       drupal_set_message('Error: ' . $parsed_json->{'response'}->{'error'}->{'description'}, 'error');
